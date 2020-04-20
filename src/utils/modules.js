6bc1c5ff908167ffd39bdfce2c890d5c46998aff
Mike Tobia <michael.m.tobia@aexp.com>
2020-04-20 12:14:48 -0400

    feat(tests): added tests for module baseUrl getter

---
+++ src/utils/modules.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2020 American Express Travel Related Services Company, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express
+ * or implied. See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { getModuleMap } from 'holocron';
+
+// eslint-disable-next-line import/prefer-default-export
+export function getModuleBaseUrl(componentKey) {
+  const moduleBaseUrl = getModuleMap().getIn(['modules', componentKey, 'baseUrl']);
+  if (moduleBaseUrl.endsWith('/')) return moduleBaseUrl.slice(0, -1);
+  return moduleBaseUrl;
+}
